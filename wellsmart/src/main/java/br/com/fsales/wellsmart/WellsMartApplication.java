6b71d5de0091b63476361ad641d677e0817620e4
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-02-14 00:36:44 -0300

    commit inicial

---
+++ wellsmart/src/main/java/br/com/fsales/wellsmart/WellsMartApplication.java
@@ -0,0 +1,13 @@
+package br.com.fsales.wellsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellsMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellsMartApplication.class, args);
+	}
+
+}
---
+++ wellsmart/src/test/java/br/com/fsales/wellsmart/WellsMartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fsales.wellsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellsMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
