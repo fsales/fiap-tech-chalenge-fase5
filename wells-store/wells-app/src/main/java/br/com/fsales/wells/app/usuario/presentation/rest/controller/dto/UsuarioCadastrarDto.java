f8b927921555be7d14f8d981cabdd38531ec1990
fsales <fabio.oliveira.sales@gmail.com>
2024-02-23 14:18:50 -0300

    criando usuario

--- wells-store/wells-app/src/main/java/br/com/fsales/wells/app/usuario/presentation/rest/controller/dto/UsuarioCadastrarDto.java
+++ wells-store/wells-app/src/main/java/br/com/fsales/wells/app/usuario/presentation/rest/controller/dto/UsuarioCadastrarDto.java
@@ -1,7 +1,15 @@
 package br.com.fsales.wells.app.usuario.presentation.rest.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public record UsuarioCadastrarDto(
-        String username,
-        String password
+        @NotEmpty
+        @Email(message = "formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+        String usuario,
+        @NotEmpty
+        @Size(min = 6, max = 6, message = "A senha deve ter no 6 caracteres")
+        String senha
 ) {
 }
