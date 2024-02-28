d7b05aae3aff907659bd2e6ea6fa25917c89af87
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-02-28 19:21:11 -0300

    Feature/usuario (#15)
    * Usuario
    
    * Update .env
    
    * Usuario

--- wells-store/wells-core/src/main/java/br/com/fsales/wells/core/util/EmailUtil.java
+++ wells-store/wells-core/src/main/java/br/com/fsales/wells/core/util/EmailUtil.java
@@ -13,7 +13,6 @@ public final class EmailUtil {
      * @return
      */
     public static boolean isValidEmail(@NonNull String email) {
-
         String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         return Pattern.matches(regex, email);
     }
