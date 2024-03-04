a2fb57e8e33726b34356788cb3f1b22c04e7ea47
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-03 23:12:50 -0300

    adicioando anotação @EnableMethodSecurity para validar a role do usuário

--- wells-store/wells-usuario/src/main/java/br/com/wells/app/infrastructure/spring/config/security/SecurityConfigurations.java
+++ wells-store/wells-usuario/src/main/java/br/com/wells/app/infrastructure/spring/config/security/SecurityConfigurations.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfigurations {
 
     private static final String[] DOCUMENTATION_OPENAPI = {
---
+++ wells-store/wells-usuario/src/main/resources/db/changelog/changes/postgres/V3__DML.sql
@@ -0,0 +1,5 @@
+INSERT INTO wells.usuario (username, senha, role, data_criacao)
+VALUES
+    ('cliente@wellsstore.br', '$2a$10$TD/.6nyjJl8UDIoLGVvARO0sQykrvXzotbTEx8Lfh2h6IlQ4yu9my', 'ROLE_CLIENTE', CURRENT_TIMESTAMP),
+    ('admin@wellsstore.br', '$2a$10$TD/.6nyjJl8UDIoLGVvARO0sQykrvXzotbTEx8Lfh2h6IlQ4yu9my', 'ROLE_ADMIN', CURRENT_TIMESTAMP);
+
