3594c4305b528c54099d531bcf635b935f4d7483
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-20 11:47:41 -0300

    ajuste

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,9 +3,15 @@
 # Inicializa o SDKMAN
 source /root/.sdkman/bin/sdkman-init.sh
 
+# Verifica se o SDKMAN foi inicializado corretamente
+sdk version
+
 # Instala a última versão do Amazon Corretto 17 e define como padrão
 sdk install java 17.0.0-amzn
 sdk default java 17.0.0-amzn
+
+# Verifica se o Java foi instalado corretamente
+java -version
 
 # Obtém a versão do Java 11 que está instalada
 java11_version=$(sdk list java | grep ' 11.' | awk '{print $NF}')
