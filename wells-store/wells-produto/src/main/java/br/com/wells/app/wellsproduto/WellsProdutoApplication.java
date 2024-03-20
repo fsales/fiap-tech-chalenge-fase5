7ca040130f1c842b2eddaba27fb99b31623b7040
vinicius-Sdr <vini.santana.612@gmail.com>
2024-03-20 15:54:12 -0300

    firt commit produto

---
+++ wells-store/wells-produto/src/main/java/br/com/wells/app/wellsproduto/WellsProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.wells.app.wellsproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellsProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellsProdutoApplication.class, args);
+    }
+
+}
