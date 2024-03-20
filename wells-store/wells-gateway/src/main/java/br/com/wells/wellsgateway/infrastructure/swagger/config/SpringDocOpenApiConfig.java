fb44e2c56b27c02fd98f7a6cf5fd515b9e7fcbba
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-20 01:14:03 -0300

    configurando o swagger

--- wells-store/wells-gateway/src/main/java/br/com/wells/wellsgateway/infrastructure/swagger/config/SpringDocOpenApiConfig.java
+++ wells-store/wells-gateway/src/main/java/br/com/wells/wellsgateway/infrastructure/swagger/config/SpringDocOpenApiConfig.java
@@ -1,14 +1,31 @@
 package br.com.wells.wellsgateway.infrastructure.swagger.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.wells.wellsgateway.infrastructure.swagger.properties.SwaggerConfigProperties;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
+@EnableWebFlux
 public class SpringDocOpenApiConfig {
+
+	final RouteDefinitionLocator locator;
+
+	public SpringDocOpenApiConfig(RouteDefinitionLocator locator) {
+		this.locator = locator;
+	}
 
 	@Bean
 	public OpenAPI openAPI(SwaggerConfigProperties swaggerConfigProperties) {
@@ -19,5 +36,37 @@ public class SpringDocOpenApiConfig {
 			.version(application.version())
 			.license(new License().name("MIT License")));
 	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		// corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfig.setAllowedOrigins(corsConfig.getAllowedOrigins());
+		corsConfig.setMaxAge(3600L);
+		corsConfig.addAllowedMethod("*");
+		corsConfig.addAllowedHeader("Requestor-Type");
+		corsConfig.addExposedHeader("X-Get-Header");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
+
+	// @Bean
+	// public List<GroupedOpenApi> apis() {
+	// List<GroupedOpenApi> groups = new ArrayList<>();
+	// List<RouteDefinition> definitions =
+	// locator.getRouteDefinitions().collectList().block();
+	// assert definitions != null;
+	// definitions.stream()
+	// .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+	// .forEach(routeDefinition -> {
+	// String name = routeDefinition.getId().replaceAll("-service", "");
+	// groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name +
+	// "/**").group(name).build());
+	// });
+	// return groups;
+	// }
 
 }
--- wells-store/wells-usuario/src/main/java/br/com/wells/usuario/app/infrastructure/config/swagger/SpringDocOpenApiConfig.java
+++ wells-store/wells-usuario/src/main/java/br/com/wells/usuario/app/infrastructure/config/swagger/SpringDocOpenApiConfig.java
@@ -12,6 +12,9 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @OpenAPIDefinition
@@ -21,7 +24,8 @@ public class SpringDocOpenApiConfig {
 	public OpenAPI openAPI(SwaggerConfigProperties swaggerConfigProperties) {
 		var application = swaggerConfigProperties.getApplication();
 
-		return new OpenAPI().servers(List.of(new Server().url(swaggerConfigProperties.getUrl())))
+		return new OpenAPI()
+			// .servers(List.of(new Server().url(swaggerConfigProperties.getUrl())))
 			.components(new Components().addSecuritySchemes("security", securityScheme()))
 			.info(new Info().title(application.title())
 				.description(application.description())
@@ -36,6 +40,21 @@ public class SpringDocOpenApiConfig {
 			.scheme("bearer")
 			.bearerFormat("JWT")
 			.name("security");
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(config.getAllowedOrigins());
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		source.registerCorsConfiguration("/wells-usuario/v3/api-docs", config);
+		return new CorsFilter(source);
 	}
 
 }
