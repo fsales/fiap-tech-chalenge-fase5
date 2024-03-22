5c49c0c6bb806a86d94eddf020d0a560a6aaba2f
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-22 09:46:10 -0300

    criando servico de pagamento

--- wells-store/wells-core/src/main/java/br/com/wells/core/domain/pagamento/model/Pagamento.java
+++ wells-store/wells-core/src/main/java/br/com/wells/core/domain/pagamento/model/Pagamento.java
@@ -1,17 +1,31 @@
 package br.com.wells.core.domain.pagamento.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import br.com.wells.core.domain.pagamento.model.enumeration.StatusPagamento;
 import br.com.wells.core.domain.pagamento.model.enumeration.TipoCartao;
-import br.com.wells.core.domain.usuario.exception.PagamentoInvalidoException;
 
 public record Pagamento(Long id, double valor, String nome, String numero, LocalDate expiracao, String codigo,
 		StatusPagamento status, TipoCartao tipoCartao, Long carrinhoId) {
-	public Pagamento {
-		if (nome == null || numero == null || expiracao == null || codigo == null || status == null
-				|| tipoCartao == null || carrinhoId == null) {
-			throw new PagamentoInvalidoException("Todos os campos obrigatórios devem ser fornecidos.");
-		}
-	}
+    public Pagamento {
+        Objects.requireNonNull(id,"O campo 'id' é obrigatório.");
+        Objects.requireNonNull(nome, "O campo 'nome' é obrigatório.");
+        Objects.requireNonNull(numero, "O campo 'numero' é obrigatório.");
+        Objects.requireNonNull(expiracao, "O campo 'expiracao' é obrigatório.");
+        Objects.requireNonNull(codigo, "O campo 'codigo' é obrigatório.");
+        Objects.requireNonNull(status, "O campo 'status' é obrigatório.");
+        Objects.requireNonNull(tipoCartao, "O campo 'tipoCartao' é obrigatório.");
+        Objects.requireNonNull(carrinhoId, "O campo 'carrinhoId' é obrigatório.");
+
+        validarCampoNaoVazio(nome, "nome");
+        validarCampoNaoVazio(numero, "numero");
+        validarCampoNaoVazio(codigo, "codigo");
+    }
+
+    private void validarCampoNaoVazio(String valor, String nomeCampo) {
+        if (valor.trim().isEmpty()) {
+            throw new IllegalArgumentException("O campo '" + nomeCampo + "' não pode estar em branco.");
+        }
+    }
 }
