7886c86cce9d41687d7cffee6f8f3ee281e3551d
Fábio Sales <fabio.oliveira.sales@gmail.com>
2024-03-26 23:35:44 -0300

    corrigindo dockerfile

--- wells-store/wells-gateway/src/main/java/br/com/wells/wellsgateway/infrastructure/swagger/config/SpringDocOpenApiConfig.java
+++ wells-store/wells-gateway/src/main/java/br/com/wells/wellsgateway/infrastructure/swagger/config/SpringDocOpenApiConfig.java
@@ -53,18 +53,20 @@ public class SpringDocOpenApiConfig {
 	// return new CorsWebFilter(source);
 	// }
 
-//	@Bean
-//	public List<GroupedOpenApi> apis() {
-//		List<GroupedOpenApi> groups = new ArrayList<>();
-//		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-//		assert definitions != null;
-//		definitions.stream()
-//			.filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
-//			.forEach(routeDefinition -> {
-//				String name = routeDefinition.getId().replaceAll("-service", "");
-//				groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-//			});
-//		return groups;
-//	}
+	// @Bean
+	// public List<GroupedOpenApi> apis() {
+	// List<GroupedOpenApi> groups = new ArrayList<>();
+	// List<RouteDefinition> definitions =
+	// locator.getRouteDefinitions().collectList().block();
+	// assert definitions != null;
+	// definitions.stream()
+	// .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+	// .forEach(routeDefinition -> {
+	// String name = routeDefinition.getId().replaceAll("-service", "");
+	// groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name +
+	// "/**").group(name).build());
+	// });
+	// return groups;
+	// }
 
 }
